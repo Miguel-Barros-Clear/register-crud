650d65dbb6de80f6599c895711398f0a32958cc5
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-06 09:07:28 -0300

    feat: footer component

--- frontend/src/components/template/footer.jsx
+++ frontend/src/components/template/footer.jsx
@@ -2,5 +2,13 @@ import "./footer.css";
 import React from "react";
 
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer className="footer">
+      <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+        <strong> Cod</strong>
+        <span className="text-danger">3</span>r
+      </span>
+    </footer>
+  );
 }
