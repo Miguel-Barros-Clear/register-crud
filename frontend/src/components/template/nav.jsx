4e462d3fd96b9d23230663eb5d99ba899763b975
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-06 09:14:31 -0300

    feat: navigation component

--- frontend/src/components/template/nav.jsx
+++ frontend/src/components/template/nav.jsx
@@ -2,5 +2,16 @@ import "./nav.css";
 import React from "react";
 
 export default function Nav(props) {
-  return <aside className="menu-area">Navegação</aside>;
+  return (
+    <aside className="menu-area">
+      <nav className="menu">
+        <a href="/">
+          <i className="fa fa-home"></i> Inicio
+        </a>
+        <a href="/users">
+          <i className="fa fa-users"></i> Usuarios
+        </a>
+      </nav>
+    </aside>
+  );
 }
