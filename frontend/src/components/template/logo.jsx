b9c68b17e6fb25f8a8d5634096d4e7da9554a078
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-06 08:50:26 -0300

    feat: component logo

--- frontend/src/components/template/logo.jsx
+++ frontend/src/components/template/logo.jsx
@@ -2,5 +2,16 @@ import "./logo.css";
 import React from "react";
 
 export default function Logo(props) {
-  return <aside className="logo">Logo</aside>;
+  return (
+    <aside className="logo">
+      <a href="/" className="logo">
+        <img
+          src={
+            "https://import.cdn.thinkific.com/220759%2Fcustom_site_themes%2Fid%2FhbIbe45tSJSfztUcZ1Qm_LOGOTIPO-CODER-FUNDOTRANSPARENTE-PRETA.png?width=384&dpr=1"
+          }
+          alt=""
+        />
+      </a>
+    </aside>
+  );
 }
